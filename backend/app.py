6fa3379f7f1513c5571995864356ef13b8f65817
khadidja2005 <mk_brakta@esi.dz>
2024-10-01 17:36:10 +0100

    frontend set up

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from Langchain import process_user_input
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class InputText(BaseModel):
     input_text: str
